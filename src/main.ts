96e09f91666a8617778566e5dcbb0a09d3f1efd3
Omar Tantawy <o.tantawy.o@gmail.com>
2025-01-31 21:45:32 +0200

    Updated Physics in main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,13 @@ const config: Types.Core.GameConfig = {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x:0, y: 300 },
+            debug: false
+        }
+    },
     scene: [
         Boot,
         Preloader,
