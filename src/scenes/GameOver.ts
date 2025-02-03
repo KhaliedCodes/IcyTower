6cc9789282340af900b3607d149fab459eeb27a0
Omar Tantawy <o.tantawy.o@gmail.com>
2025-02-03 18:27:24 +0200

    Update GameOver.ts

--- src/scenes/GameOver.ts
+++ src/scenes/GameOver.ts
@@ -1,10 +1,12 @@
-import { Scene } from 'phaser';
+import { Scene ,GameObjects } from 'phaser';
 
 export class GameOver extends Scene
 {
     camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
     gameover_text : Phaser.GameObjects.Text;
+    mainButton: GameObjects.Text;
+    exitButton: GameObjects.Text;
+
 
     constructor ()
     {
@@ -16,9 +18,6 @@ export class GameOver extends Scene
         this.camera = this.cameras.main
         this.camera.setBackgroundColor(0xff0000);
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
-
         this.gameover_text = this.add.text(512, 384, 'Game Over', {
             fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
@@ -26,9 +25,31 @@ export class GameOver extends Scene
         });
         this.gameover_text.setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
+        this.mainButton = this.add.text(this.scale.width / 2, 600, 'Main Menu', {
+            fontFamily: 'Verdana', fontSize: 36, color: '#ffffff',
+            backgroundColor: '#000000', padding: { x: 30, y: 15 },
+            align: 'center'
+        }).setOrigin(0.5).setInteractive({ useHandCursor: true });
+
+        this.mainButton.on('pointerdown', () => {
             this.scene.start('MainMenu');
             window.location.reload();
+        });
+        
+
+        this.exitButton = this.add.text(this.scale.width / 2, 700, 'Exit', {
+            fontFamily: 'Verdana', fontSize: 36, color: '#ffffff',
+            backgroundColor: '#000000', padding: { x: 30, y: 15 },
+            align: 'center'
+        }).setOrigin(0.5).setInteractive({ useHandCursor: true });
+
+        this.exitButton.on('pointerdown', () => {
+            window.close();
+        });
+
+        [this.mainButton, this.exitButton].forEach(button => {
+            button.on('pointerover', () => button.setStyle({ backgroundColor: '#942222' }));
+            button.on('pointerout', () => button.setStyle({ backgroundColor: button === this.mainButton ? '#000000' : '#000000' }));
         });
     }
 }
